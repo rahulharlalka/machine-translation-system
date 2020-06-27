033a14e34b7fcffc9d88ff2e8c6dd6a34a27660e
rahul harlalka <rahulharlalka96@gmail.com>
2020-06-27 14:19:56 +0530

    server file completed moving to testing

--- server.py
+++ server.py
@@ -6,6 +6,20 @@ from load_model import load_vars
 import enc_dec_construct
 app=Flask(__name__)
 
+
+def load_defaults():
+    global essentials,model = load_vars()    
+    global encoder_model,decoder_model = enc_dec_construct.construct(model)
+    global num_encoder_tokens=essentials['num_encoder_tokens']
+    global num_decoder_tokens=essentials['num_decoder_tokens']
+    global max_encoder_seq_length=essentials['max_encoder_seq_length']
+    global max_decoder_seq_length=essentials['max_decoder_seq_length']
+    global input_token_index=essentials['input_token_index']
+    global target_token_index=essentials['target_token_index']
+    global reverse_input_char_index=essentials['reverse_input_char_index']
+    global reverse_target_char_index=essentials['reverse_target_char_index']
+    del essentials
+
 def decode_sequence(input_seq):
     states_value = encoder_model.predict(input_seq)
 
@@ -34,31 +48,30 @@ def decode_sequence(input_seq):
 
     return decoded_sentence
 
-
-def load_defaults():
-    global essentials,model = load_vars()    
-    global encoder_model,decoder_model = enc_dec_construct.construct(model)
-    global num_encoder_tokens=essentials['num_encoder_tokens']
-    global num_decoder_tokens=essentials['num_decoder_tokens']
-    global max_encoder_seq_length=essentials['max_encoder_seq_length']
-    global max_decoder_seq_length=essentials['max_decoder_seq_length']
-    global input_token_index=essentials['input_token_index']
-    global target_token_index=essentials['target_token_index']
-    global reverse_input_char_index=essentials['reverse_input_char_index']
-    global reverse_target_char_index=essentials['reverse_target_char_index']
-    del essentials
+def get_encoder_input_data(input_texts):
+    encoder_input_data = np.zeros((len(input_texts), max_encoder_seq_length, num_encoder_tokens),dtype='float32')
+    for i , input_text in enumerate(input_texts):
+        for t,char in enumerate(input_text):
+            encoder_input_data[i, t, input_token_index[char]] = 1.
+        encoder_input_data[i, t + 1:, input_token_index[' ']] = 1.
+    return encoder_input_data
 
 
 
+@app.route('/translate',methods=["POST"])
+def get_sentence():
+    sentence=str(request.form['english'])
+    encoder_input_data=get_encoder_input_data(sentence)
+    decoded_sentence=decode_sequence(encoder_input_data)
+    return decoded_sentence
 
-@app.route('/translate' ,methods=["POST"])
-def hello_world():
-    return 'code under maintainance'
+
+
+@app.route('/')
+def default():
+    return "logged into wrong page : log into http://localhost:5000/translate"
 
 if __name__=='__main__':
-    load_defaults()
-    
-
-
+    load_defaults() 
     app.run(debug=True)
     
